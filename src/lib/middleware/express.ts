49c63ef08583270321d52d860e1ebf102e0d831d
Steven Bennett <steven@budgetbloom.com>
2022-04-02 18:03:12 -0700

    Quick release - patch

--- src/lib/middleware/express.ts
+++ src/lib/middleware/express.ts
@@ -4,13 +4,13 @@ import { NextFunction, Request, Response } from 'express';
 import { DateTime } from 'luxon';
 import { Logger } from '../logger';
 
-const getActualRequestDurationInMilliseconds = (start: [number, number]) => {
-  const NS_PER_SEC = 1e9; //  convert to nanoseconds
-  const NS_TO_MS = 1e6; // convert to milliseconds
-  const diff = process.hrtime(start);
+// const getActualRequestDurationInMilliseconds = (start: [number, number]) => {
+//   const NS_PER_SEC = 1e9; //  convert to nanoseconds
+//   const NS_TO_MS = 1e6; // convert to milliseconds
+//   const diff = process.hrtime(start);
 
-  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
-};
+//   return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+// };
 
 export function expressLoggerMiddleware(
   logger: Logger,
@@ -19,7 +19,7 @@ export function expressLoggerMiddleware(
   next: NextFunction,
 ) {
   if (logger) {
-    const start = process.hrtime();
+    // const start = process.hrtime();
     const now = DateTime.now();
     const requestId = req.headers['X-Request-Id']?.toString();
 
